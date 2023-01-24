493ba897644c8ebd2c14d20d6bf5dc907475c836
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-24 13:09:05 +0700

    Adding initial import, useState to Home.jsx :sparkles:

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
+import { Loader, Card, FormField } from '../components';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328]">
+          The Community Showcase
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
