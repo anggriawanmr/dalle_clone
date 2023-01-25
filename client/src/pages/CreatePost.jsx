802ff8d2b5d89e193727cb6128332c55749d022a
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-25 10:02:07 +0700

    Adding initial header on CreatePost page :sparkles:

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,7 +16,17 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  return <div>CreatePost</div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+          Create imaginative and visually stunning images through DALL-E AI and
+          share them with the community
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
