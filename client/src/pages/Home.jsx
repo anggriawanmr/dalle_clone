356bc3f2c846636f38006a1e735c3a1ca7db6fa1
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-25 09:57:22 +0700

    Adding utils index.js :sparkles:

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -46,12 +46,9 @@ const Home = () => {
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards
-                  data="searchResults"
-                  title="No search results found"
-                />
+                <RenderCards data={[]} title="No search results found" />
               ) : (
-                <RenderCards data="allPosts" title="No Posts found" />
+                <RenderCards data={[]} title="No Posts found" />
               )}
             </div>
           </>
