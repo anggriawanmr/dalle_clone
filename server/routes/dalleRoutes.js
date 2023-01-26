a6a4277580e8be77de20c34dee5b629486b6e3dc
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-27 01:43:37 +0700

    Finalizing dalleRoutes :hammer:

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -12,8 +12,24 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-router.route('/').get((req, res) => {
-  res.send('Hello from DALaaaL-E!');
+router.route('/').get(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
 });
 
 export default router;
